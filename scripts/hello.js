8370610d81eb58cd79f3bf89daf7898811fb6d0d
vorn\mizuho-kono <mizuho-kono@vorn.co.jp>
2021-11-21 17:03:52 +0900

    変更

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,9 @@
 'use strict';
 module.exports = robot => {
-  robot.hear(/hello>/i, msg => {
+  robot.hear(/おみくじ>/i, msg => {
     const user_id = msg.message.user.id;
-    msg.send(`Hello, <@${user_id}>`);
+    const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+    const lot = lots[Math.floor(Math.random() * lots.length)];
+    msg.send(`${lot}, <@${user_id}>`);
   });
 };
